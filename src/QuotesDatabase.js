c53e193720c7b96bb8aa9280e2c7b0d89aff37cb
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-01-04 14:40:13 +0100

    added some quotes + upgrade serialize js

--- src/QuotesDatabase.js
+++ src/QuotesDatabase.js
@@ -3,19 +3,19 @@ const quotes=[
   "quote": "J'ai souvent envie de demander aux femmes par quoi elles remplacent l'intelligence",
   "author": "Alain"
   }, {
-  "quote": "une bonne affaire, acheter toutes les femmes aux prix qu'elles valent et les revendre aux prix qu'elles s'estiment",
+  "quote": "Une bonne affaire : acheter toutes les femmes aux prix qu'elles valent et les revendre aux prix qu'elles s'estiment",
   "author": "Jules Renard"
   }, {
-  "quote": "Aimer les femmes intelligentes est un plaisir de pederastre",
+  "quote": "Aimer les femmes intelligentes est un plaisir de pédérastre",
   "author": "Charles Baudelaire"
   }, {
-  "quote": "J'ai tjs été étonné qu'on puisse laisser entrer des femmes dans l'église, quelles conversations peuvent-elles tenir avec Dieu",
+  "quote": "J'ai toujours été étonné qu'on puisse laisser entrer des femmes dans l'église... Quelles conversations peuvent-elles tenir avec Dieu",
   "author": "Charles Baudelaire"
   }, {
   "quote": "Les femmes sont un sexe purement decoratif, elles n'ont jamais rien à dire mais elles le disent de façon charmante.",
   "author": "Oscar Wilde"
   }, {
-  "quote": "Je n'ai jamais eu de chances avec les femmes, il y a tjs un moment où leur betise a depassé mon amour.",
+  "quote": "Je n'ai jamais eu de chances avec les femmes, il y a toujours un moment où leur bétise a depassé mon amour.",
   "author": "Léautaud"
   }, {
   "quote": "Il n'y a que dans les courts instants où la femme ne pense plus a ce qu'elle dit, qu'elle dit vraiment ce qu'elle pense.",
@@ -27,14 +27,41 @@ const quotes=[
   "quote": "Les femmes ne sont que des organes génitaux articulés et dotés de la faculté de dépenser tout l'argent que l'on possède",
   "author": "Faulkner"
   }, {
-  "quote": "C'est nous qui faisons des femmes ce qu'elles valent et voila pq elles ne valent rien.",
+  "quote": "C'est nous qui faisons des femmes ce qu'elles valent et voila pourquoi elles ne valent rien.",
   "author": "Mirabeau"
   }, {
-  "quote": "Les femmes acceptent aisément les idées nouvelles, car elles sont ignorantes. Elles les repandent facilement parce-qu'elles sont légères, elles les soutiennent longtemps parce-qu'elles sont tétues",
+  "quote": "Les femmes acceptent aisément les idées nouvelles, car elles sont ignorantes. Elles les répandent facilement parce-qu'elles sont légères, elles les soutiennent longtemps parce-qu'elles sont tétues",
   "author": "Mirabeau"
   }, {
-  "quote": "Les femmes ressemblent a des girouettes, elles se fixent qd elles se rouillent",
+  "quote": "Les femmes ressemblent à des girouettes, elles se fixent qd elles se rouillent",
   "author": "Voltaire"
+  }, {
+  "quote": "Misogyne, c'est-à-dire amoureux de la première venue.",
+  "author": "Jules Renard"
+  }, {
+  "quote": "La femme est une surface qui mime la profondeur.",
+  "author": "Friedrich Nietzsche"
+  }, {
+  "quote": "Une vraie femme sait qu'elle doit être dominée.",
+  "author": "André Suarès"
+  }, {
+  "quote": "Les femmes sont les instruments interchangeables d'un plaisir toujours identique.",
+  "author": "Marcel Proust"
+  }, {
+  "quote": "Les caprices de l'espèce femelle ne sont pas bornés à une seule partie du monde ni à un seul climat, mais sont en tous lieux les mêmes.",
+  "author": "Jonathan Swift"
+  }, {
+  "quote": "Le ventre des femmes recèle toujours un enfant ou une maladie.",
+  "author": "Louis-Ferdinand Céline"
+  }, {
+  "quote": "Il y a un principe bon qui crée l'ordre, la lumière et l'homme. Il y a un principe mauvais qui crée le chaos, les ténèbres et la femme.",
+  "author": "Pythagore"
+  }, {
+  "quote": "Une femme infidèle est comme une grande boucherie de viande froide, dont on ne veut pas, parce que quelqu'un y a déjà touché.",
+  "author": "Anton Tchekhov"
+  }, {
+  "quote": "Une femme qui exerce son intelligence devient laide, folle et guenon.",
+  "author": "Pierre Joseph Proudhon"
   }
 ]
 export default quotes
